7a819ed1e2551535424453a3d2e00c28e7dbd496
Bert Van den Bosch <bert@fotovdb.com>
2025-10-27 22:15:07 +0100

    Bitlist data structure

--- src/core/BitList.hpp
+++ src/core/BitList.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
+#include "Intrinsics.hpp"
 #include "core.hpp"
 #include <cassert>
+
+#define BATCH_SEARCH 1
 
 /*
  * Free list helper DS.
@@ -9,7 +12,7 @@
  * 0|false:	free slot
  * 1|true:	taken
  */
-template <u32 N, typename WordType = u8>
+template <u32 N, typename WordType = u8, typename BatchWordType = u32>
 struct BitList final
 {
     static constexpr u32 BitsPerWord = sizeof(WordType) * 8u;
@@ -40,29 +43,40 @@ struct BitList final
         u32 word_index, bit_index;
         get_indices(index, word_index, bit_index);
 
-        u32 mask = ~(1 << bit_index);
+        WordType mask = ~(1 << bit_index);
 
         data[word_index] &= mask;
     }
 
+    /*
+     * Find first flag starting from the given index. start_index excluded.
+     */
     i32 find_first(bool flag, u32 start_index = 0u) const
     {
-        u32 word_index, bit_index;
-        get_indices(start_index, word_index, bit_index);
+        u32 word_start_index, bit_start_index;
+        get_indices(start_index, word_start_index, bit_start_index);
 
-        for (u32 word_i = word_index; word_i < NumWords; word_i++)
+        for (u32 word_i = word_start_index; word_i < NumWords; word_i++)
         {
-            // TODO(Bert): use intrinsics to do this in a single instruction
-            // instead of for every bit in the word
-            const u32 bit_start_index = (word_i == word_index) ? bit_index : 0u;
-            for (u32 bit_i = bit_start_index; bit_i < BitsPerWord; bit_i++)
+            const u32 bit_start_offset =
+                word_i == word_start_index ? bit_start_index : 0u;
+
+            WordType word = data[word_i] >> bit_start_offset;
+
+            word = flag ? word : ~word;
+
+            i32 set_index = -1;
+            if constexpr (BitsPerWord <= 32u)
             {
-                const u32 mask  = 1u << bit_i;
-                const u32 value = flag ? data[word_i] : ~data[word_i];
-                if (value & mask)
-                {
-                    return bit_i + (word_i * BitsPerWord);
-                }
+                set_index = Intrinsics::find_lsb((u32)word);
+            }
+            else
+            {
+                set_index = Intrinsics::find_lsb((u64)word);
+            }
+            if (set_index >= 0)
+            {
+                return set_index + bit_start_offset + (word_i * BitsPerWord);
             }
         }
 
@@ -87,7 +101,7 @@ struct BitList final
         u32 word_index, bit_index;
         get_indices(index, word_index, bit_index);
 
-        u32 mask = 1 << bit_index;
+        WordType mask = 1 << bit_index;
 
         return (data[word_index] & mask);
     }
---
+++ src/core/Intrinsics.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+#include "core.hpp"
+
+// #include <immintrin.h>
+
+namespace Intrinsics
+{
+
+constexpr i32 find_lsb(u32 value)
+{
+    if (value == 0u)
+    {
+        return -1;
+    }
+#if defined(__clang__) || defined(__GNUC__)
+    return __builtin_ctz(value);
+#else
+    return -1;
+#endif
+}
+
+constexpr i32 find_lsb(u64 value)
+{
+    if (value == 0u)
+    {
+        return -1;
+    }
+#if defined(__clang__) || defined(__GNUC__)
+    return __builtin_ctzll(value);
+#else
+    return -1;
+#endif
+}
+
+constexpr i32 find_msb(u32 value)
+{
+    if (value == 0u)
+    {
+        return -1;
+    }
+#if defined(__clang__) || defined(__GNUC__)
+    return __builtin_clz(value);
+#else
+    return -1;
+#endif
+}
+
+// constexpr i32 find_msb(u64 value)
+// {
+//     if (value == 0u)
+//     {
+//         return -1;
+//     }
+// #if defined(__clang__) || defined(__GNUC__)
+// 	return __builtin_clzll(value);
+// #endif
+// }
+
+} // namespace Intrinsics
